efce933c0ec7046cd7deb7eefaa2a29f49edc47d
ibrahimaliyevv2 <7caucb@code.edu.az>
2024-08-13 19:53:54 +0400

    added

---
+++ src/App-v0.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 5 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
+  }
+
+  render() {
+    const date = new Date("12 February 2008");
+    date.setDate(date.getDate() + this.state.count);
+
+    return (
+      <div>
+        <h1>React App</h1>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
+
+export default App;
